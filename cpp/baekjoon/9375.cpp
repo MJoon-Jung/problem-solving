9b98de502c144546c276d5a36df7d9aba01bc20a
MJoon-Jung <gjgjajaj31@gmail.com>
2022-06-28 12:43:19 +0900

    baekjoon problem

--- cpp/baekjoon/9375.cpp
+++ cpp/baekjoon/9375.cpp
@@ -1,18 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m;
-string s, t;
+int n,m;
+string a, b;
 int main()
 {
   ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
   cin >> n;
   for(int i = 0; i < n; i++) {
+    map<string, int> map;
     cin >> m;
     for(int j = 0; j < m; j++) {
-      cin >> s >> t;
-      
+      cin >> a >> b;
+      map[b]++; 
     }
+    long long ret = 1;
+    for(auto z: map) {
+      ret *= ((long long)(z.second + 1));
+    }
+    ret--;
+    cout << ret << "\n";
   }
-
   return 0;
 }
